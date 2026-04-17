784ea3cc7010b7a005997a15ef3f90f24c50908f
Diego Fernando Salazar Rosero <dfsalazarr@siesa.com>
2026-04-17 08:11:28 -0500

    se suben cambio de  funcionan los accesos rápidos.

--- backend/FundacionPanorama.API/obj/Debug/net10.0/FundacionPanorama.API.AssemblyInfo.cs
+++ backend/FundacionPanorama.API/obj/Debug/net10.0/FundacionPanorama.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FundacionPanorama.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101833002d3a5a3a93e3f51e87bbb63bf6b411f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea02426a272f75b4e4b2fba8cc87960c1aa2b18")]
 [assembly: System.Reflection.AssemblyProductAttribute("FundacionPanorama.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FundacionPanorama.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
